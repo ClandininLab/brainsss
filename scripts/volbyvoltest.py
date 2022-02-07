9fdabd43c0e27d9e32a7e4b1b0d46f62ddddb983
Luke Brezovec <luke@DN51stpr.SUNet>
2022-02-06 17:22:01 -0800

    testing vol by vol strategy

--- scripts/volbyvoltest.py
+++ scripts/volbyvoltest.py
@@ -24,15 +24,15 @@ def main(args):
     brain_dims = img.header.get_data_shape()
 
     # calculate the meanbrain, which will be fixed in moco
-    meanbrain = np.zeros(brain_dims[:3])
-    for i in range(brain_dims[-1]):
-        meanbrain += img.dataobj[...,i]
-    meanbrain = meanbrain/brain_dims[-1] # divide by number of volumes
-    fixed = ants.from_numpy(np.asarray(meanbrain, dtype='float32'))
-    printlog('meanbrain DONE')
+    # meanbrain = np.zeros(brain_dims[:3])
+    # for i in range(brain_dims[-1]):
+    #     meanbrain += img.dataobj[...,i]
+    # meanbrain = meanbrain/brain_dims[-1] # divide by number of volumes
+    # fixed = ants.from_numpy(np.asarray(meanbrain, dtype='float32'))
+    # printlog('meanbrain DONE')
 
     # Make empty hdf5 file to append processed volumes to with matching shape
-    save_file = '/oak/stanford/groups/trc/data/Brezovec/20220204_test.h5'
+    save_file = '/oak/stanford/groups/trc/data/Brezovec/20220207_test.h5'
     with h5py.File(save_file, 'w') as f:
         dset = f.create_dataset('data', (*brain_dims[:3],0), maxshape=(*brain_dims[:3],None), dtype='float32')
     printlog('created empty hdf5 file')
@@ -44,13 +44,13 @@ def main(args):
         # Load a single brain volume
         vol = img.dataobj[...,i]
         
-        ### Process vol (moco, zscore, etc) ###
-        # Make ants image
-        moving = ants.from_numpy(np.asarray(vol, dtype='float32'))
+        # ### Process vol (moco, zscore, etc) ###
+        # # Make ants image
+        # moving = ants.from_numpy(np.asarray(vol, dtype='float32'))
 
-        # Motion correct
-        moco = ants.registration(fixed,moving,type_of_transform='SyN')
-        moco_out = moco['warpedmovout'].numpy()
+        # # Motion correct
+        # moco = ants.registration(fixed,moving,type_of_transform='SyN')
+        # moco_out = moco['warpedmovout'].numpy()
 
         # Append to hdf5 file
         with h5py.File(save_file, 'a') as f:
@@ -61,7 +61,7 @@ def main(args):
             f['data'].resize(new_num_vol,axis=3) # increase size by one volume
             
             # Append to hdf5 file
-            f['data'][...,-1] = moco_out
+            f['data'][...,-1] = vol #moco_out
         printlog(F'vol: {i}, time: {time()-t0}')
 
 if __name__ == '__main__':
