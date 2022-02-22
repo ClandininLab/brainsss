9501b6aa385472acb99d67934e2d0609b92220a7
Luke Brezovec <brezovec@stanford.edu>
2022-02-22 08:37:50 -0800

    testing user config and argument passing

---
+++ scripts/20220222_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=testing
+#SBATCH --partition=trc
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=./logs/mainlog.out
+#SBATCH --open-mode=append
+
+ml python/3.6.1
+date
+python3 -u ./20220222_test.py
