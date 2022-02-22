bd91c5e062b04d02f8d5ac8683a4ee542a76942c
Luke Brezovec <brezovec@stanford.edu>
2022-02-22 11:50:36 -0800

    testing user config and argument passing

--- scripts/20220222_test.py
+++ scripts/20220222_test.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("PWD")
-    parser.add_argument("dataset_path", default="MISSING")
+    parser.add_argument("dataset_path", default="MISSING", required=False)
     args = parser.parse_args()
     print("hi")
     if args.dataset_path == "MISSING":
