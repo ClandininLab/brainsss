cf49d084891aa18083a34e59f571023f5e4d0bbc
Luke Brezovec <brezovec@stanford.edu>
2022-02-28 10:18:00 -0800

    making shared preprocessing script

---
+++ scripts/motion_correction.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=moco
+#SBATCH --partition=trc
+#SBATCH --time=4-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --output=./logs/mainlog.out
+#SBATCH --open-mode=append
+
+directory=$1
+master_brain=$2
+mirror_brain=$3
+
+echo $directory
+echo $master_brain
+echo $mirror_brain
+
+args="{directory:$directory, master_brain:$master_brain, mirror_brain:$mirror_brain}"
+
+ml python/3.6
+date
+python3 -u ./motion_correction.py $args
