7a5cf5f64449380b8facffa25bb49c0b1238b40e
yandanw <yandanw@stanford.edu>
2022-03-01 16:19:36 -0800

    HW1

---
+++ scripts/hw_toy_model.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import numpy as np
+import argparse
+import json
+from time import time
+import brainsss
+
+def main(args):
+    logfile = args['logfile']
+    printlog = getattr(brainsss.Printlog(logfile=logfile), 'print_to_log')
+    a = args['a']
+    b = args ['b']
+    c = args ['c']
+    d = args['d']
+    printlog("{} plus {} is {}".format(a,b,c))
+
+if __name__ == '__main__':
+    main(json.loads(sys.argv[1]))
