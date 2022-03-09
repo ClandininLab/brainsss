9ee45ff2f2ab56c9b9abf022f6cf67a2e21e14ec
Luke Brezovec <brezovec@stanford.edu>
2022-03-08 17:24:16 -0800

    stim triggered beh

--- brainsss/visual.py
+++ brainsss/visual.py
@@ -5,100 +5,100 @@ import pandas as pd
 
 
 def pd_csv_to_h5py(directory, file):
-    """ Loads photodiode data from csv file and saves to h5py file.
+	""" Loads photodiode data from csv file and saves to h5py file.
 
-    Parameters
-    ----------
-    directory: full path to vision folder
-    file: csv file
+	Parameters
+	----------
+	directory: full path to vision folder
+	file: csv file
 
-    Returns
-    -------
-    Nothing. """
+	Returns
+	-------
+	Nothing. """
 
-    print('loading raw photodiode data... ',end='')
-    #load raw data from csv
-    load_file = os.path.join(directory, file)
-    temp = np.genfromtxt(load_file, delimiter=',',skip_header=1)
-    t = temp[:,0]
-    ft_triggers = temp[:,1]
-    pd1 = temp[:,2]
-    pd2 = temp[:,3]
-    print('done')
+	print('loading raw photodiode data... ',end='')
+	#load raw data from csv
+	load_file = os.path.join(directory, file)
+	temp = np.genfromtxt(load_file, delimiter=',',skip_header=1)
+	t = temp[:,0]
+	ft_triggers = temp[:,1]
+	pd1 = temp[:,2]
+	pd2 = temp[:,3]
+	print('done')
 
-    #save as h5py file
-    print('saving photodiode data as h5py file...',end='')
-    save_file = os.path.join(directory, 'photodiode.h5')
-    with h5py.File(save_file, 'w') as hf:
-        hf.create_dataset('time',  data=t)
-        hf.create_dataset('ft_triggers',  data=ft_triggers)
-        hf.create_dataset('pd1',  data=pd1)
-        hf.create_dataset('pd2',  data=pd2)
-    print('done')
-    
+	#save as h5py file
+	print('saving photodiode data as h5py file...',end='')
+	save_file = os.path.join(directory, 'photodiode.h5')
+	with h5py.File(save_file, 'w') as hf:
+		hf.create_dataset('time',  data=t)
+		hf.create_dataset('ft_triggers',  data=ft_triggers)
+		hf.create_dataset('pd1',  data=pd1)
+		hf.create_dataset('pd2',  data=pd2)
+	print('done')
+	
 def load_h5py_pd_data(directory):
-    """ Loads photodiode data from h5py file.
+	""" Loads photodiode data from h5py file.
 
-    Parameters
-    ----------
-    directory: full path to vision folder
+	Parameters
+	----------
+	directory: full path to vision folder
 
-    Returns
-    -------
-    t: 1D numpy array, times of photodiode measurement (in ms)
-    pd1: 1D numpy array, photodiode 1 measurements
-    pd2: 1D numpy array, photodiode 1 measurements """
+	Returns
+	-------
+	t: 1D numpy array, times of photodiode measurement (in ms)
+	pd1: 1D numpy array, photodiode 1 measurements
+	pd2: 1D numpy array, photodiode 1 measurements """
 
-    print('loading photodiode data... ',end='')
-    #load from h5py file
-    load_file = os.path.join(directory, 'photodiode.h5')
-    with h5py.File(load_file, 'r') as hf:
-        t = hf['time'][:]
-        ft_triggers = hf['ft_triggers'][:]
-        pd1 = hf['pd1'][:]
-        pd2 = hf['pd2'][:]
-    print('done')
-    return t, ft_triggers, pd1, pd2
+	print('loading photodiode data... ',end='')
+	#load from h5py file
+	load_file = os.path.join(directory, 'photodiode.h5')
+	with h5py.File(load_file, 'r') as hf:
+		t = hf['time'][:]
+		ft_triggers = hf['ft_triggers'][:]
+		pd1 = hf['pd1'][:]
+		pd2 = hf['pd2'][:]
+	print('done')
+	return t, ft_triggers, pd1, pd2
 
 def load_photodiode(vision_path):
-    """ Tries to load photodiode data from h5py file, and if it doesn't exist loads from csv file.
+	""" Tries to load photodiode data from h5py file, and if it doesn't exist loads from csv file.
 
-    Parameters
-    ----------
-    vision_path: full path to vision folder
+	Parameters
+	----------
+	vision_path: full path to vision folder
 
-    Returns
-    -------
-    t: 1D numpy array, times of photodiode measurement (in ms)
-    pd1: 1D numpy array, photodiode 1 measurements
-    pd2: 1D numpy array, photodiode 1 measurements """
+	Returns
+	-------
+	t: 1D numpy array, times of photodiode measurement (in ms)
+	pd1: 1D numpy array, photodiode 1 measurements
+	pd2: 1D numpy array, photodiode 1 measurements """
 
-    # Try to load from h5py file
-    try:
-        t, ft_triggers, pd1, pd2 = load_h5py_pd_data(vision_path)
-        
-    # First convert from csv to h5py, then load h5py
-    except:
-        pd_csv_to_h5py(vision_path,'photodiode.csv')
-        t, ft_triggers, pd1, pd2 = load_h5py_pd_data(vision_path)
-    return t, ft_triggers, pd1, pd2
+	# Try to load from h5py file
+	try:
+		t, ft_triggers, pd1, pd2 = load_h5py_pd_data(vision_path)
+		
+	# First convert from csv to h5py, then load h5py
+	except:
+		pd_csv_to_h5py(vision_path,'photodiode.csv')
+		t, ft_triggers, pd1, pd2 = load_h5py_pd_data(vision_path)
+	return t, ft_triggers, pd1, pd2
 
 def get_metadata_from_visprotocol(file, series):
 	stim_ids = []
 	angles = []
 	with h5py.File(file, 'r') as f:
 		series = list(f['Flies']['0']['epoch_runs'].keys())
-	    epoch_ids = f['Flies']['0']['epoch_runs'][series].get('epochs').keys()
-	    print(len(epoch_ids))
-	    for i, epoch_id in enumerate(epoch_ids):
-	        stim_id = f['Flies']['0']['epoch_runs'][series].get('epochs').get(epoch_id).attrs['component_stim_type']
-	        stim_ids.append(stim_id)
-	        if stim_id == 'DriftingSquareGrating':
-	            angle = f['Flies']['0']['epoch_runs'][series].get('epochs').get(epoch_id).attrs['angle']
-	            angles.append(angle)
-	            #starts.append(stimulus_start_times[i])
-	        else:
-	        	angles.append(None)
+		epoch_ids = f['Flies']['0']['epoch_runs'][series].get('epochs').keys()
+		print(len(epoch_ids))
+		for i, epoch_id in enumerate(epoch_ids):
+			stim_id = f['Flies']['0']['epoch_runs'][series].get('epochs').get(epoch_id).attrs['component_stim_type']
+			stim_ids.append(stim_id)
+			if stim_id == 'DriftingSquareGrating':
+				angle = f['Flies']['0']['epoch_runs'][series].get('epochs').get(epoch_id).attrs['angle']
+				angles.append(angle)
+				#starts.append(stimulus_start_times[i])
+			else:
+				angles.append(None)
 	return stim_ids, angles
 
 def extract_stim_times_from_pd(photodiode_trace, time_vector):
