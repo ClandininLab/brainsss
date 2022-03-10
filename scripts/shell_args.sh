1ecff5b4ced62f104018e8a7fbcef8e88fdf612f
Luke Brezovec <brezovec@stanford.edu>
2022-03-10 12:40:55 -0800

    shell args

--- scripts/shell_args.sh
+++ scripts/shell_args.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-POSITIONAL_ARGS=()
-
 while [[ $# -gt 0 ]]; do
   case $1 in
     -e|--extension)
@@ -22,16 +20,9 @@ while [[ $# -gt 0 ]]; do
       echo "Unknown option $1"
       exit 1
       ;;
-    *)
-      POSITIONAL_ARGS+=("$1") # save positional arg
-      shift # past argument
-      ;;
   esac
 done
-
-set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
 echo "FILE EXTENSION  = ${EXTENSION}"
 echo "SEARCH PATH     = ${SEARCHPATH}"
 echo "DEFAULT         = ${DEFAULT}"
-echo "POSITIONAL_ARGS = ${POSITIONAL_ARGS}"
