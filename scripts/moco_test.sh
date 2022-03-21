cb73dc670eb6a07be7cb115feedf2a9d3f975087
Max Turner <maxwellholteturner@gmail.com>
2022-03-21 13:41:33 -0700

    Moco optional params motion_correction.py takes new optional params: type_of_transform, flow_sigma & total_sigma passed to ants registration; and output_format, which can be nii (instead of default h5) to save registered images as nii files, and delete intermediate h5 files

--- scripts/moco_test.sh
+++ scripts/moco_test.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-#SBATCH --job-name=moco
-#SBATCH --time=2-00:00:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --output=./logs/mainlog.out
-#SBATCH --open-mode=append
+# !/bin/bash
+# SBATCH --job-name=moco
+# SBATCH --time=2-00:00:00
+# SBATCH --ntasks=1
+# SBATCH --cpus-per-task=8
+# SBATCH --output=./logs/mainlog.out
+# SBATCH --open-mode=append
 
 date
 
@@ -25,6 +25,6 @@ echo $output_format
 args="{\"directory\":\"$directory\",\"brain_master\":\"$brain_master\",\"brain_mirror\":\"$brain_mirror\","\
 "\"type_of_transform\":\"$type_of_transform\",\"output_format\":\"$output_format\"}"
 
-ml python/3.6 antspy/0.2.2
+# ml python/3.6 antspy/0.2.2
 
 python3 -u ./motion_correction.py $args
