6f7bb955880ccb0b06c850215720f4ef563c26db
Luke Brezovec <brezovec@stanford.edu>
2022-04-01 00:52:57 -0700

    added meanbrain post

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -11,58 +11,62 @@ while [[ $# -gt 0 ]]; do
   case $1 in
     -b|--build_flies)
       BUILDFLIES="$2"
-      shift # past argument
-      shift # past value
+      shift
+      shift
       ;;
     -f|--flies)
       FLIES="$2"
-      shift # past argument
-      shift # past value
+      shift
+      shift
       ;;
     -d|--dirtype)
       DIRTYPE="$2"
-      shift # past argument
-      shift # past value
+      shift
+      shift
       ;;
     -m|--moco)
       MOCO=True
-      shift # past argument
+      shift
       ;;
     -z|--zscore)
       ZSCORE=True
-      shift # past argument
+      shift
       ;;
     -h|--highpass)
       HIGHPASS=True
-      shift # past argument
+      shift
       ;;
     -c|--correlation)
       CORRELATION=True
-      shift # past argument
+      shift
       ;;
     --fictrac_qc)
       FICTRAC_QC=True
-      shift # past argument
+      shift
       ;;
-    --STB) # stimulus triggered behavior
+    --STB)
       STB=True
-      shift # past argument
+      shift
       ;;
-    --bleaching_qc) # stimulus triggered behavior
+    --bleaching_qc)
       BLEACHING_QC=True
-      shift # past argument
+      shift
       ;;
-    --temporal_mean_brain) # stimulus triggered behavior
-      TEMPORAL_MEAN_BRAIN=True
-      shift # past argument
+    --temporal_mean_brain_pre)
+      TEMPORAL_MEAN_BRAIN_PRE=True
+      shift
       ;;
-    --STA) # stimulus triggered behavior
+    --temporal_mean_brain_post)
+      TEMPORAL_MEAN_BRAIN_POST=True
+      shift
+      ;;
+    --STA)
       STA=True
-      shift # past argument
+      shift
       ;;
-    --H5_TO_NII) # stimulus triggered behavior
+    --H5_TO_NII)
       H5_TO_NII=True
-      shift # past argument
+      shift
       ;;
     -*|--*)
       echo "Unknown option $1"
@@ -71,7 +75,11 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-ARGS="{\"PWD\":\"$PWD\",\"BUILDFLIES\":\"$BUILDFLIES\",\"FLIES\":\"$FLIES\",\"DIRTYPE\":\"$DIRTYPE\",\"MOCO\":\"$MOCO\",\"ZSCORE\":\"$ZSCORE\",\"HIGHPASS\":\"$HIGHPASS\",\"CORRELATION\":\"$CORRELATION\",\"FICTRAC_QC\":\"$FICTRAC_QC\",\"STB\":\"$STB\",\"BLEACHING_QC\":\"$BLEACHING_QC\",\"TEMPORAL_MEAN_BRAIN\":\"$TEMPORAL_MEAN_BRAIN\",\"STA\":\"$STA\",\"H5_TO_NII\":\"$H5_TO_NII\"}"
+ARGS="{\"PWD\":\"$PWD\",\"BUILDFLIES\":\"$BUILDFLIES\",\"FLIES\":\"$FLIES\",\"DIRTYPE\":\"$DIRTYPE\",
+\"MOCO\":\"$MOCO\",\"ZSCORE\":\"$ZSCORE\",\"HIGHPASS\":\"$HIGHPASS\",\"CORRELATION\":\"$CORRELATION\",
+\"FICTRAC_QC\":\"$FICTRAC_QC\",\"STB\":\"$STB\",\"BLEACHING_QC\":\"$BLEACHING_QC\",
+\"TEMPORAL_MEAN_BRAIN_PRE\":\"$TEMPORAL_MEAN_BRAIN_PRE\",\"STA\":\"$STA\",\"H5_TO_NII\":\"$H5_TO_NII\",
+\"TEMPORAL_MEAN_BRAIN_POST\":\"$TEMPORAL_MEAN_BRAIN_POST\"}"
 
 ml python/3.6
 date
