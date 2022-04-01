894bc0cc7a925b1bd6ab537675f6fe5ed2d0fd9f
Luke Brezovec <brezovec@stanford.edu>
2022-04-01 00:19:32 -0700

    save STA maxproj

--- scripts/stim_triggered_avg_neu.py
+++ scripts/stim_triggered_avg_neu.py
@@ -63,11 +63,15 @@ def main(args):
 			printlog(F"Slice: {slice_num}. Duration: {time()-t0}")
 		stas_array = np.asarray(stas)
 
+		### SAVE STA ###
 		savedir = os.path.join(func_path, 'STA')
 		if not os.path.exists(savedir):
 			os.mkdir(savedir)
-		savefile = os.path.join(savedir, F'sta_{angle}')
+		savefile = os.path.join(savedir, F'sta_{angle}.npy')
 		np.save(savefile,stas_array)
+
+		### SAVE PNG ###
+		save_maxproj_img(savefile)
 
 def load_slice(brain_path, slice_num):
 	with h5py.File(brain_path, 'r') as hf:
@@ -111,6 +115,17 @@ def make_stas(ynew,new_stim_timestamps,chunk_edges,bin_start=20,bin_end=55):
 			running_sum += section
 	sta = running_sum / np.size(new_stim_timestamps)
 	return sta
+
+def save_maxproj_img(file):
+
+	data = np.load(file)
+	data = np.nan_to_num(data)
+
+	plt.figure(figsize=(10,4))
+	plt.imshow(np.max(np.max(data,axis=0),axis=-1).T)
+
+	save_file = file[:-3] + 'png'
+	plt.savefig(save_file, bbox_inches='tight', dpi=300)
 
 if __name__ == '__main__':
 	main(json.loads(sys.argv[1]))
