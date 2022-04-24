fc3d239ea05c9d7014e58409e380c50f21e8491b
Luke Brezovec <brezovec@stanford.edu>
2022-04-24 14:29:24 -0700

    updated warp dir

--- scripts/apply_transforms.py
+++ scripts/apply_transforms.py
@@ -50,9 +50,12 @@ def main(args):
     affine_file = os.listdir(os.path.join(save_directory, 'func-to-anat_fwdtransforms_2umiso'))[0]
     affine_path = os.path.join(save_directory, 'func-to-anat_fwdtransforms_2umiso', affine_file)
 
-    syn_files = os.listdir(os.path.join(save_directory, 'anat-to-meanbrain_fwdtransforms_2umiso'))
-    syn_linear_path = os.path.join(save_directory, 'anat-to-meanbrain_fwdtransforms_2umiso', [x for x in syn_files if '.mat' in x][0])
-    syn_nonlinear_path = os.path.join(save_directory, 'anat-to-meanbrain_fwdtransforms_2umiso', [x for x in syn_files if '.nii.gz' in x][0])
+
+    warp_dir = 'anat-to-non_myr_mean_fwdtransforms_2umiso'
+    #warp_dir = 'anat-to-meanbrain_fwdtransforms_2umiso'
+    syn_files = os.listdir(os.path.join(save_directory, warp_dir))
+    syn_linear_path = os.path.join(save_directory, warp_dir, [x for x in syn_files if '.mat' in x][0])
+    syn_nonlinear_path = os.path.join(save_directory, warp_dir, [x for x in syn_files if '.nii.gz' in x][0])
 
     transforms = [affine_path, syn_linear_path, syn_nonlinear_path]
 
