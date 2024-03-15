c43c03fb4103e2ca94841b850a5883c36a7b5362
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2024-03-14 18:18:42 -0700

    messed up

--- scripts/background_subtraction.py
+++ scripts/background_subtraction.py
@@ -148,6 +148,7 @@ def main(args):
         dims = np.shape(data)
 
         printlog(full_load_path)
+        printlog(save_directory)
         printlog("Data shape is {}".format(dims))  
         br = BgRemover3D(full_load_path, save_directory, half_wid=5)
 
