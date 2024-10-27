c25b922ce2fbd4719a6870f6decbecf014acd848
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2024-10-27 12:20:15 -0700

    comments added

--- scripts/raw_warp.py
+++ scripts/raw_warp.py
@@ -42,19 +42,19 @@ def main(args):
         dims = np.shape(data)
 
         printlog("Data shape is {}".format(dims))
-        printlog('RAM memory used::{}'.format(psutil.virtual_memory()[2]))
-        printlog('RAM Used (GB)::{}'.format(psutil.virtual_memory()[3]/1000000000))
+        # printlog('RAM memory used::{}'.format(psutil.virtual_memory()[2]))
+        # printlog('RAM Used (GB)::{}'.format(psutil.virtual_memory()[3]/1000000000))
         
         #QC fig of raw data
-        save_file = os.path.join(save_directory, 'raw_brain.nii')
-        nib.Nifti1Image(data, np.eye(4)).to_filename(save_file)
-        printlog("Saved {}".format(save_file))
-        brain_img = np.asarray(nib.load(save_file).get_data().squeeze(), dtype='float32')
+        save_img = os.path.join(save_directory, 'raw_brain.nii')
+        nib.Nifti1Image(data, np.eye(4)).to_filename(save_img)
+        printlog("Saved {}".format(save_img))
+        brain_img = np.asarray(nib.load(save_img).get_data().squeeze(), dtype='float32')
         plt.figure(figsize=(10,4))
         plt.imshow(np.max(brain_img[:,:,20,:],axis=-1).T,cmap='gray')
         plt.axis('off')
-        save_file_f = save_file[:-3] + 'png'
-        plt.savefig(save_file_f, bbox_inches='tight', dpi=300)
+        save_img_f = save_img[:-3] + 'png'
+        plt.savefig(save_img_f, bbox_inches='tight', dpi=300)
         
         #Warp in chunks so we don't run out of memory, this is creating the stepsize of chunks
         stepsize = 100
@@ -67,15 +67,15 @@ def main(args):
         #Warp the brain
         warped = warp_raw(data=data, steps=steps, fixed=fixed, func_path=fly_directory)
         printlog("Warped brain shape is {}".format(np.shape(warped)))
-        printlog('RAM memory used::{}'.format(psutil.virtual_memory()[2]))
-        printlog('RAM Used (GB)::{}'.format(psutil.virtual_memory()[3]/1000000000))
+        # printlog('RAM memory used::{}'.format(psutil.virtual_memory()[2]))
+        # printlog('RAM Used (GB)::{}'.format(psutil.virtual_memory()[3]/1000000000))
     
         #Save the warped brain
         with h5py.File(save_file, "w") as data_file:
             data_file.create_dataset("data", data=warped.astype('float32'))
-        printlog('RAM memory used::{}'.format(psutil.virtual_memory()[2]))
-        printlog('RAM Used (GB)::{}'.format(psutil.virtual_memory()[3]/1000000000))
-    
+        # printlog('RAM memory used::{}'.format(psutil.virtual_memory()[2]))
+        # printlog('RAM Used (GB)::{}'.format(psutil.virtual_memory()[3]/1000000000))
+    printlog("Data saved in {}".format(save_file))
 if __name__ == '__main__':
     main(json.loads(sys.argv[1]))
 
