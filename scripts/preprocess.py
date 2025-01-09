4cb4ae08906912c668d530bdbc9cf544d7cdaee7
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-01-08 17:48:10 -0800

    add mem to timestamp warp

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -720,7 +720,7 @@ def main(args):
                 logfile=logfile,
                 time=2,
                 cpus=32,
-                mem='200GB',
+                mem='250GB',
                 nice=nice,
                 nodes=nodes,
                 # global_resources=True, 
