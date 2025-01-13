2f145da64fbc5f30f4f263546bc0f200dbdaf673
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-01-13 15:42:52 -0800

    adding a bin_size for calc

--- scripts/filter_bins.py
+++ scripts/filter_bins.py
@@ -85,12 +85,14 @@ def main(args):
                     bin_idx[...,chunkstart:chunkend] = np.digitize(ts_chunk, bins_array) ### Added nan to num because if a pixel is a constant value (over saturated) will divide by 0
                     # printlog(F"vol: {chunkstart} to {chunkend}")
         # bin_idx = np.digitize(ts, bins_array)
+        bin_size = bin_end-bin_start
         
         #save filter_needs
         filter_needs_file = os.path.join(save_directory, 'filter_needs.h5')
         with h5py.File(filter_needs_file, "w") as data_file:
                     data_file.create_dataset("bins", data=bin_idx)
                     data_file.create_dataset("loom_starts", data=starts_loom_ms)
+                    data_file.create_dataset("bin_size", data=bin_size)
         
         printlog(f"Array for temp filter done. Data saved in {filter_needs_file}")
 if __name__ == '__main__':
