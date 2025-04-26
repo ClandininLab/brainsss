907d5a8359f3a809e4571aadaa5dc9fbac3b5c2c
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-04-26 16:51:52 -0700

    timed out

--- scripts/postprocess.sh
+++ scripts/postprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=postpro
 #SBATCH --partition=trc
-#SBATCH --time=10:00:00
+#SBATCH --time=4-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --output=./logs/mainlog.out
