695e1794a175f20a14c3f50026dac68fd933a833
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-05-02 17:02:25 -0700

    ts_warp only needs to be made once

--- scripts/timestamp_warp.py
+++ scripts/timestamp_warp.py
@@ -38,48 +38,51 @@ def main(args):
     printlog("Beginning TIMESTAMP WARP")
     
     #Timestamps need to be warped as well, load them here    
-    timestamps = brainsss.load_timestamps(load_directory)    
-    printlog("Timestamp shape is {}".format(np.shape(timestamps)))
-    
-    #Load mean brain
-    fixed = brainsss.load_fda_meanbrain()
-    
-    #Get the values of the differences between each timestamp to create a relative timestamp matrix
-    relative=timestamps[0]-timestamps[0][0]
-    
-    #Create a list of the relative timestamps
-    vals=[]
-    for ts in range(np.shape(timestamps)[0]):
-        val=timestamps[ts]-relative
-        vals.append(val[0])
-    
-    #Create a matrix of the relative timestamps for each frame, should be same shape as the data
-    x=256 #I know I shouldn't hard code this but it seems a waste of mem to load the brain just to get this....
-    y=128
-    ts_xl=[]
-    for val in relative:
-        fframe=np.zeros((x,y))
-        fframe.fill(val)
-        ts_xl.append(fframe)
-    ts_xl=np.array(ts_xl)
-    ts_xl=np.moveaxis(ts_xl,0,-1)
-    printlog("New timestamp shape is {}".format(np.shape(ts_xl)))
-    
-    #Warp this extra large timestamp matrix
-    warped_ts = warp_raw(data=ts_xl, stepsize=None, fixed=fixed, func_path=fly_directory)
-    
-    #Add the relative timestamps to the warped timestamps to get the absolute timestamps
-    total_ts=[]
-    for i in range(np.shape(vals)[0]):
-        temp_ts=warped_ts+vals[i]
-    #     print(np.shape(temp_ts))
-        total_ts.append(temp_ts)
-    total_ts=np.array(total_ts)
-    total_ts=np.moveaxis(total_ts,0,-1)
-    printlog("Warped timestamp shape is {}".format(np.shape(total_ts)))
-    
-    with h5py.File(save_file, "w") as data_file:
-            data_file.create_dataset("data", data=total_ts.astype('float32'))
+    if save_file not in os.listdir(save_directory):
+        timestamps = brainsss.load_timestamps(load_directory)    
+        printlog("Timestamp shape is {}".format(np.shape(timestamps)))
+        
+        #Load mean brain
+        fixed = brainsss.load_fda_meanbrain()
+        
+        #Get the values of the differences between each timestamp to create a relative timestamp matrix
+        relative=timestamps[0]-timestamps[0][0]
+        
+        #Create a list of the relative timestamps
+        vals=[]
+        for ts in range(np.shape(timestamps)[0]):
+            val=timestamps[ts]-relative
+            vals.append(val[0])
+        
+        #Create a matrix of the relative timestamps for each frame, should be same shape as the data
+        x=256 #I know I shouldn't hard code this but it seems a waste of mem to load the brain just to get this....
+        y=128
+        ts_xl=[]
+        for val in relative:
+            fframe=np.zeros((x,y))
+            fframe.fill(val)
+            ts_xl.append(fframe)
+        ts_xl=np.array(ts_xl)
+        ts_xl=np.moveaxis(ts_xl,0,-1)
+        printlog("New timestamp shape is {}".format(np.shape(ts_xl)))
+        
+        #Warp this extra large timestamp matrix
+        warped_ts = warp_raw(data=ts_xl, stepsize=None, fixed=fixed, func_path=fly_directory)
+        
+        #Add the relative timestamps to the warped timestamps to get the absolute timestamps
+        total_ts=[]
+        for i in range(np.shape(vals)[0]):
+            temp_ts=warped_ts+vals[i]
+        #     print(np.shape(temp_ts))
+            total_ts.append(temp_ts)
+        total_ts=np.array(total_ts)
+        total_ts=np.moveaxis(total_ts,0,-1)
+        printlog("Warped timestamp shape is {}".format(np.shape(total_ts)))
+        
+        with h5py.File(save_file, "w") as data_file:
+                data_file.create_dataset("data", data=total_ts.astype('float32'))
+    else:
+        printlog("Warped timestamps already created")
     
 if __name__ == '__main__':
     main(json.loads(sys.argv[1]))
