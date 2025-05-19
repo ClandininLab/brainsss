2e3fbba6df1b25361bac2e89ec69ed70c58df0c2
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-05-19 01:32:32 -0700

    updating watcher

--- scripts/check_and_submit.sh
+++ scripts/check_and_submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define the job name or ID you want to check
-JOB_NAME="prepro"
+JOB_NAME="postpro"
 
 # Check if the job is running
 if ! squeue -u $USER | grep -q "$JOB_NAME"; then
     echo "Job $JOB_NAME not running. Submitting a new job..."
-    sbatch preprocess.sh -f fly_250 -fb -rts -tf
+    sbatch postprocess.sh -post -e 10flies -best
 else
     echo "Job $JOB_NAME is still running."
 fi
