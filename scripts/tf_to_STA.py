19e3f8e62ef652f003eae73b79f50f147e4a875e
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-06-21 08:56:11 -0700

    check files have been moved

--- scripts/tf_to_STA.py
+++ scripts/tf_to_STA.py
@@ -64,35 +64,38 @@ def main(args):
                     tf_files.append(file)
                     save_file= os.path.join(temp_dir, behavior, f'STA_{cc}_{behavior}_{steps}.h5')
         printlog(f"Following files to process: {tf_files}")
-        for file in tf_files:
-            fly_val=int(file.split("_")[0])
-            if fly_val in flies:
-                load_path = os.path.join(behave_dir,file)
-                printlog(f"being processes {load_path}")
-                temp=[]
-                with h5py.File(load_path, 'r+') as hf:    
-                        ts = hf['time_stamps'][:]
-                        brain = hf['brain']
-                        dimst = np.shape(ts)
-                        dims = np.shape(brain)
-                        # printlog(f'Brain shape: {dims}, time stamp shape: {dimst}')
-                        for i in range(range_start, range_end, steps):
-                            end = i + steps if i + steps < range_end else range_end
-                            mask = (ts > i) & (ts < end)
-                            result = np.nanmean(np.where(mask, brain, np.nan),axis=-1)
-                            temp.append(result)
-                # printlog(f'Temp shape is {np.shape(temp)}')
-                STA.append(temp)
-                # printlog(f'STA is {np.shape(STA)}')
-            else:
-                printlog(f'Fly {fly_val} not in {flies}')
-        STA=np.asarray(np.nanmean(STA, axis=0))
-    
-        # printlog(f'Saving STA to {save_file}')
-        with h5py.File(save_file, "w") as data_file:
-                data_file.create_dataset("data", data=STA.astype('float32'))
+        if tf_files:
+            for file in tf_files:
+                fly_val=int(file.split("_")[0])
+                if fly_val in flies:
+                    load_path = os.path.join(behave_dir,file)
+                    printlog(f"being processes {load_path}")
+                    temp=[]
+                    with h5py.File(load_path, 'r+') as hf:    
+                            ts = hf['time_stamps'][:]
+                            brain = hf['brain']
+                            dimst = np.shape(ts)
+                            dims = np.shape(brain)
+                            # printlog(f'Brain shape: {dims}, time stamp shape: {dimst}')
+                            for i in range(range_start, range_end, steps):
+                                end = i + steps if i + steps < range_end else range_end
+                                mask = (ts > i) & (ts < end)
+                                result = np.nanmean(np.where(mask, brain, np.nan),axis=-1)
+                                temp.append(result)
+                    # printlog(f'Temp shape is {np.shape(temp)}')
+                    STA.append(temp)
+                    # printlog(f'STA is {np.shape(STA)}')
+                else:
+                    printlog(f'Fly {fly_val} not in {flies}')
+            STA=np.asarray(np.nanmean(STA, axis=0))
         
-        printlog(f"STA for {behavior} done. Data saved in {save_file}")
+            # printlog(f'Saving STA to {save_file}')
+            with h5py.File(save_file, "w") as data_file:
+                    data_file.create_dataset("data", data=STA.astype('float32'))
+            
+            printlog(f"STA for {behavior} done. Data saved in {save_file}")
+        else:
+            printlog(f"No files found for {behavior}")
 if __name__ == '__main__':
     main(json.loads(sys.argv[1]))
 
