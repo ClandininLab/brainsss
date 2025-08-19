7f8debc86db4cd57b5f90a36af1da584e0cb1f01
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-08-19 11:02:39 -0700

    making labels the same

--- scripts/supercluster.py
+++ scripts/supercluster.py
@@ -37,48 +37,40 @@ def main(args):
     super_clust = clust_num
     
     behave_dict_path=os.path.join(temp_dir,f'behave_dict_total_{event}.pkl')
-    with open(behave_dict_path, 'rb') as file:
-        behave_dict_total = pickle.load(file)
-        behaviors=list(behave_dict_total.keys())
-        printlog(f'behaviors are {list(behave_dict_total.keys())}')
+    giant_vox_labels = np.load(os.path.join(cluster_dir, 'cluster_labels_best_flies.npy'))
+    full_res = brainsss.sta_to_full_res(behave_dict_path, giant_vox_labels, super_vox)
     
-    giant_cluster_labels = np.load(os.path.join(cluster_dir, 'cluster_labels_best_flies.npy'))
-    supervox_dict_total=brainsss.make_supervox_dict(behave_dict_total, behaviors, super_vox,giant_cluster_labels)
-
-    full_res={}
-    for behave in supervox_dict_total:
-        supervox_brain=np.moveaxis(supervox_dict_total[behave],-1,-2)
-        full_res_brain=brainsss.supervoxel_to_full_res(supervox_brain, giant_cluster_labels, super_vox)
-        full_res_brain=np.moveaxis(full_res_brain, 0,-1)
-        full_res_brain=np.moveaxis(full_res_brain, 0,-1)
-        full_res[behave]=np.asarray(full_res_brain)
-        
-
     printlog('clustering.........')
-    superclust_labels_dict={}
+    giant_cluster_labels = np.load(os.path.join(cluster_dir, f'supercluster_labels_total_{super_clust}.npy'))
+    if not os.path.exists(giant_cluster_labels):
+        total_dict_path=os.path.join(temp_dir,'behave_dict_total_10flies.pkl')
+        full_res_total = brainsss.sta_to_full_res(total_dict_path, giant_vox_labels, super_vox)
+        brain_total=full_res_total['total']
+        shape=np.shape(brain_total)
+
+        connectivity = grid_to_graph(shape[0],shape[1],shape[2]).astype('float32')
+        neural_activity_total= brain_total.reshape(-1, shape[-1])
+
+        giant_cluster_labels= []
+        cluster_model= AgglomerativeClustering(connectivity=connectivity,
+                                            n_clusters=super_clust,
+                                            memory=None,
+                                            linkage='ward')
+
+        cluster_model.fit(neural_activity_total)
+        cluster_labels = np.asarray(cluster_model.labels_)
+        printlog(f'shape of cluster labels {np.shape(cluster_labels)}')
+        
+        np.save(os.path.join(cluster_dir, f'supercluster_labels_total_{super_clust}.npy'), giant_cluster_labels)
+    
     superclust_dict = {}
     for behavior in full_res:
         brain=full_res[behavior]
-        shape=np.shape(brain)
-        
-        connectivity = grid_to_graph(shape[0],shape[1],shape[2]).astype('float32')
-        neural_activity= brain.reshape(-1, shape[-1])
-        
-        cluster_labels= []
-        cluster_model= AgglomerativeClustering(connectivity=connectivity,
-                                               n_clusters=super_clust,
-                                               memory=None,
-                                               linkage='ward')
-        
-        cluster_model.fit(neural_activity)
-        cluster_labels = np.asarray(cluster_model.labels_)
-        printlog(f'shape of cluster labels {np.shape(cluster_labels)}')
-        superclust_labels_dict[behavior]=cluster_labels
-        printlog(f'done with {behavior} trial labels')
+        neural_activity= brain.reshape(-1, np.shape(brain)[-1])
 
         behavior_superclusters = []
         for cluster_num in range(super_clust):
-            labels= superclust_labels_dict[behavior]
+            labels= giant_cluster_labels
             cluster_indicies= np.where(labels==cluster_num)[0]
             mean_signal = np.mean(neural_activity[cluster_indicies,:], axis=0)
             behavior_superclusters.append(mean_signal)
@@ -90,12 +82,7 @@ def main(args):
         gc.collect()
         printlog(f'done with {behavior} superclustering')
     
-    save_file_labels = os.path.join(cluster_dir, f'superclust_labels_{super_clust}_{event}.pkl')
     save_file_clusters = os.path.join(cluster_dir, f'superclust_clusters_{super_clust}_{event}.pkl')
-    
-    printlog(f'Saving labels to {save_file_labels}')
-    with open(save_file_labels, 'wb') as file:
-        pickle.dump(superclust_labels_dict, file)
     
     printlog(f'Saving labels to {save_file_clusters}')
     with open(save_file_clusters, 'wb') as file:
