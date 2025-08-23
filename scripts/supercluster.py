b3ec0373cb34ab2fc20371b34c9c101cd5aa671b
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-08-22 18:07:05 -0700

    making sure clusters don't form outside brain

--- scripts/supercluster.py
+++ scripts/supercluster.py
@@ -39,6 +39,7 @@ def main(args):
     behave_dict_path=os.path.join(temp_dir,f'behave_dict_total_{event}.pkl')
     giant_vox_labels = np.load(os.path.join(cluster_dir, 'cluster_labels_best_flies.npy'))
     full_res = brainsss.vox_to_full_res(behave_dict_path, giant_vox_labels, super_vox)
+    fixed = brainsss.load_fda_meanbrain().numpy()
     
     printlog('clustering.........')
     giant_cluster_labels_path = os.path.join(cluster_dir, f'supercluster_labels_total_{super_clust}.npy')
@@ -46,6 +47,8 @@ def main(args):
         total_dict_path=os.path.join(temp_dir,'behave_dict_total_10flies.pkl')
         full_res_total = brainsss.vox_to_full_res(total_dict_path, giant_vox_labels, super_vox)
         brain_total=full_res_total['total']
+        brain_total=np.where(fixed[...,None]>0.1,brain_total, np.nan)
+        brain_total=np.nan_to_num(brain_total)
         shape=np.shape(brain_total)
 
         connectivity = grid_to_graph(shape[0],shape[1],shape[2]).astype('float32')
@@ -89,7 +92,7 @@ def main(args):
     
     save_file_clusters = os.path.join(cluster_dir, f'superclust_clusters_{super_clust}_{event}.pkl')
     
-    printlog(f'Saving labels to {save_file_clusters}')
+    printlog(f'Saving clusters to {save_file_clusters}')
     with open(save_file_clusters, 'wb') as file:
         pickle.dump(superclust_dict, file)
 if __name__ == '__main__':
