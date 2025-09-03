617412bf70c45e44823173253403e9bd16c27e32
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-09-03 14:05:48 -0700

    I think this shoud solve the problem of the file not being there??

--- scripts/temp_filter.py
+++ scripts/temp_filter.py
@@ -44,102 +44,104 @@ def main(args):
     ts_dest=os.path.join(scratch_dir, timestamp_file)
     if os.path.exists(brain_dest)==False: 
         b_dest = shutil.copyfile(brain_load_path, brain_dest)
+        brain_dest=b_dest
     if os.path.exists(ts_dest)==False: 
         t_dest = shutil.copyfile(ts_load_path, ts_dest)
-    printlog(f"brain in {brain_dest} and timestamp in {ts_dest}")
+        ts_dest=t_dest
+        printlog(f"brain in {brain_dest} and timestamp in {ts_dest}")
 
-    printlog("Beginning temporal filter")
-   
-    #Get behaviors
-    if event != None:
-        event_times_path = os.path.join(later_path, f'{event}_event_times_split_dic.pkl')
-    else:
-        event_times_path = os.path.join(later_path, 'event_times_split_dic.pkl')
-    with open(event_times_path, 'rb') as file:
-        event_times_struct = pickle.load(file)
-        f=list(event_times_struct.keys())[0]
-        behaviors=list(event_times_struct[f].keys())
-   
-    for behavior in behaviors:
+        printlog("Beginning temporal filter")
+    
+        #Get behaviors
         if event != None:
-            filter_load_path=os.path.join(scratch_dir, f"filter_needs_{cc}_{behavior}_{event}.h5")
-            ts_rel_load_path=os.path.join(scratch_dir, f"ts_rel_odd_mask_{cc}_{behavior}_{event}.h5")
-            save_file = os.path.join(save_directory, '5sec_' + brain_file.split('.')[0] + '_filtered_' + f'{behavior}_{event}.h5')
+            event_times_path = os.path.join(later_path, f'{event}_event_times_split_dic.pkl')
         else:
-            filter_load_path=os.path.join(scratch_dir, f"filter_needs_{cc}_{behavior}.h5")
-            ts_rel_load_path=os.path.join(scratch_dir, f"ts_rel_odd_mask_{cc}_{behavior}.h5")
-            save_file = os.path.join(save_directory, '5sec_' + brain_file.split('.')[0] + '_filtered_' + f'{behavior}.h5')
-        if os.path.exists(save_file)==False or redo:
-            #load brain
-            with h5py.File(brain_dest, 'r') as hf, \
-                h5py.File(ts_dest, 'r') as tf, \
-                h5py.File(ts_rel_load_path, 'r') as of, \
-                h5py.File(filter_load_path, 'r') as ff:
-                    
-                brain_all = hf['data']
-                ts_all = tf['data']
-                loom_all = ff['loom_starts'] 
-                bin_shape = ff['bin_shape']
-                odd_mask = of['odd_mask']   
-                ts_rel = of['ts_rel']     
+            event_times_path = os.path.join(later_path, 'event_times_split_dic.pkl')
+        with open(event_times_path, 'rb') as file:
+            event_times_struct = pickle.load(file)
+            f=list(event_times_struct.keys())[0]
+            behaviors=list(event_times_struct[f].keys())
+    
+        for behavior in behaviors:
+            if event != None:
+                filter_load_path=os.path.join(scratch_dir, f"filter_needs_{cc}_{behavior}_{event}.h5")
+                ts_rel_load_path=os.path.join(scratch_dir, f"ts_rel_odd_mask_{cc}_{behavior}_{event}.h5")
+                save_file = os.path.join(save_directory, '5sec_' + brain_file.split('.')[0] + '_filtered_' + f'{behavior}_{event}.h5')
+            else:
+                filter_load_path=os.path.join(scratch_dir, f"filter_needs_{cc}_{behavior}.h5")
+                ts_rel_load_path=os.path.join(scratch_dir, f"ts_rel_odd_mask_{cc}_{behavior}.h5")
+                save_file = os.path.join(save_directory, '5sec_' + brain_file.split('.')[0] + '_filtered_' + f'{behavior}.h5')
+            if os.path.exists(save_file)==False or redo:
+                #load brain
+                with h5py.File(brain_dest, 'r') as hf, \
+                    h5py.File(ts_dest, 'r') as tf, \
+                    h5py.File(ts_rel_load_path, 'r') as of, \
+                    h5py.File(filter_load_path, 'r') as ff:
                         
-                # loop through sections of the matricies
-                dims=np.shape(brain_all)
-                printlog(F"Brain data shape is {dims}")
-                
-                T=(ts_all[0,0,0,1]-ts_all[0,0,0,0])/1000
-                fs=1/T #sample rate, Hz
-                if loom_all.shape[0]>0:
-                    max_len=int((((bin_shape[1]-bin_shape[0])/1000)*fs)*np.shape(loom_all)[0])+100
-                else:
-                    printlog(F"No looms for this fly in {behavior}")
-                    continue
-                printlog(F"Max length of filtered data is {max_len}")
-
-                nx, ny, nz, nt = brain_all.shape
-                # n_voxels = nx * ny * nz
-
-                within_bin_brain_np = np.full((nx, ny, nz, max_len), np.nan)
-                within_bin_ts_rel_np = np.full((nx, ny, nz, max_len), np.nan)
-                #### Loop over z planes (io access is done nz times!!)
-                for z in (range(nz)):
-                    # Read in z plane
-                    plane = brain_all[:,:,z,:]
-                    plane_ts_rel = ts_rel[:,:,z,:]
-                    for x in range(nx):
-                        for y in range(ny):
-                            within_bin_vox = plane[x, y, odd_mask[x,y,z,:]]
-                            within_bin_vox_ts_rel = plane_ts_rel[x, y, odd_mask[x,y,z,:]]
+                    brain_all = hf['data']
+                    ts_all = tf['data']
+                    loom_all = ff['loom_starts'] 
+                    bin_shape = ff['bin_shape']
+                    odd_mask = of['odd_mask']   
+                    ts_rel = of['ts_rel']     
                             
-                            # Get the sorted indices of the timestamp array and sort both arrays using the sorted indices
-                            sorted_indices = np.argsort(within_bin_vox_ts_rel)
-                            within_bin_vox = within_bin_vox[sorted_indices]
-                            within_bin_vox_ts_rel = within_bin_vox_ts_rel[sorted_indices]
+                    # loop through sections of the matricies
+                    dims=np.shape(brain_all)
+                    printlog(F"Brain data shape is {dims}")
+                    
+                    T=(ts_all[0,0,0,1]-ts_all[0,0,0,0])/1000
+                    fs=1/T #sample rate, Hz
+                    if loom_all.shape[0]>0:
+                        max_len=int((((bin_shape[1]-bin_shape[0])/1000)*fs)*np.shape(loom_all)[0])+100
+                    else:
+                        printlog(F"No looms for this fly in {behavior}")
+                        continue
+                    printlog(F"Max length of filtered data is {max_len}")
 
-                            # populate the output array
-                            within_bin_brain_np[x,y,z,:len(within_bin_vox)] = within_bin_vox
-                            within_bin_ts_rel_np[x,y,z,:len(within_bin_vox)] = within_bin_vox_ts_rel
+                    nx, ny, nz, nt = brain_all.shape
+                    # n_voxels = nx * ny * nz
 
-                
-                # brain_final = np.array(brain_final)
-                # ts_final=np.array(ts_final)
-                brain_shape=np.shape(within_bin_brain_np)
-                ts_shape=np.shape(within_bin_ts_rel_np)
-                printlog(f"Temporal filtered data shape is {brain_shape} and timestamp shape is {ts_shape}")
-                
-                
-                
-                with h5py.File(save_file, "w") as data_file:
-                        data_file.create_dataset("brain", data=within_bin_brain_np.astype('float32'), compression="gzip", compression_opts=4, chunks=True, shuffle=True)
-                        data_file.create_dataset("time_stamps", data=within_bin_ts_rel_np.astype('float32'),  compression="gzip", compression_opts=4, chunks=True, shuffle=True)
-                
-                printlog(f"Temporal filtering for {behavior} is done. Data saved in {save_file}")
-                # Delete variables to free up memory
-                del brain_all, ts_all, loom_all, bin_shape, odd_mask, ts_rel, within_bin_brain_np, within_bin_ts_rel_np
-                del dims, T, fs, max_len, nx, ny, nz, nt, plane, plane_ts_rel, within_bin_vox, within_bin_vox_ts_rel, sorted_indices
-                
-                # Manually invoke the garbage collector
-                gc.collect() 
+                    within_bin_brain_np = np.full((nx, ny, nz, max_len), np.nan)
+                    within_bin_ts_rel_np = np.full((nx, ny, nz, max_len), np.nan)
+                    #### Loop over z planes (io access is done nz times!!)
+                    for z in (range(nz)):
+                        # Read in z plane
+                        plane = brain_all[:,:,z,:]
+                        plane_ts_rel = ts_rel[:,:,z,:]
+                        for x in range(nx):
+                            for y in range(ny):
+                                within_bin_vox = plane[x, y, odd_mask[x,y,z,:]]
+                                within_bin_vox_ts_rel = plane_ts_rel[x, y, odd_mask[x,y,z,:]]
+                                
+                                # Get the sorted indices of the timestamp array and sort both arrays using the sorted indices
+                                sorted_indices = np.argsort(within_bin_vox_ts_rel)
+                                within_bin_vox = within_bin_vox[sorted_indices]
+                                within_bin_vox_ts_rel = within_bin_vox_ts_rel[sorted_indices]
+
+                                # populate the output array
+                                within_bin_brain_np[x,y,z,:len(within_bin_vox)] = within_bin_vox
+                                within_bin_ts_rel_np[x,y,z,:len(within_bin_vox)] = within_bin_vox_ts_rel
+
+                    
+                    # brain_final = np.array(brain_final)
+                    # ts_final=np.array(ts_final)
+                    brain_shape=np.shape(within_bin_brain_np)
+                    ts_shape=np.shape(within_bin_ts_rel_np)
+                    printlog(f"Temporal filtered data shape is {brain_shape} and timestamp shape is {ts_shape}")
+                    
+                    
+                    
+                    with h5py.File(save_file, "w") as data_file:
+                            data_file.create_dataset("brain", data=within_bin_brain_np.astype('float32'), compression="gzip", compression_opts=4, chunks=True, shuffle=True)
+                            data_file.create_dataset("time_stamps", data=within_bin_ts_rel_np.astype('float32'),  compression="gzip", compression_opts=4, chunks=True, shuffle=True)
+                    
+                    printlog(f"Temporal filtering for {behavior} is done. Data saved in {save_file}")
+                    # Delete variables to free up memory
+                    del brain_all, ts_all, loom_all, bin_shape, odd_mask, ts_rel, within_bin_brain_np, within_bin_ts_rel_np
+                    del dims, T, fs, max_len, nx, ny, nz, nt, plane, plane_ts_rel, within_bin_vox, within_bin_vox_ts_rel, sorted_indices
+                    
+                    # Manually invoke the garbage collector
+                    gc.collect() 
                     
                 
         else:
