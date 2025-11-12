4ef6888b44f0e47c8e8bc7697469fdae30525088
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2025-11-11 19:47:22 -0800

    wrong if goddamnit amazon you lead me astray

--- scripts/regress_noise.py
+++ scripts/regress_noise.py
@@ -53,7 +53,7 @@ def main(args):
         printlog(behave)
         arr_g=behave_dict_g[behave]
         arr_r=behave_dict_r[behave]
-        if arr_g is None or arr_r is None:
+        if arr_g == [] or arr_r == []:
             printlog(f"Skipping behavior {behave} due to missing data.")
         else:
             total_behave=brainsss.dual_channel_remove_noise(arr_r, arr_g, printlog)
