4524c074cda5d2bbbcf5bf8cd0feeeb97d2c1dbb
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2026-04-12 07:47:19 -0700

    fixing time idk how to make this not hardcoded

--- scripts/tf_to_STA.py
+++ scripts/tf_to_STA.py
@@ -42,7 +42,7 @@ def main(args):
         printlog(f"Found behaviors: {behaviors}")
     
     
-    range_start=-2000; range_end=3000; steps=100
+    range_start=-3000; range_end=3000; steps=100
     printlog(f"Flies: {flies}")
     
     for behavior in behaviors:
