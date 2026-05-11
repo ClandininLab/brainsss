c91e55726bc068a4fb499aa3780e23c0d2e9bef0
Ilana Zucker-Scharff <ilanazs@stanford.edu>
2026-05-11 09:14:03 -0700

    changing time again

--- scripts/postprocess.sh
+++ scripts/postprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=postpro
 #SBATCH --partition=trc
-#SBATCH --time=1-00:00:00
+#SBATCH --time=00:12:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --output=./logs/mainlog.out
